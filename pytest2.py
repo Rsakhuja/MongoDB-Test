97b8741a5f46d3a9ea655f573f1195f665f5aa25
Rajeev Sakhuja <rajeev@Rajeevs-MBP.hsd1.nj.comcast.net>
2019-10-19 17:12:58 -0400

    ok

--- pytest2.py
+++ pytest2.py
@@ -23,6 +23,7 @@ def GetOne(client, userid):
 try:
     # Object represents a connection to the DB
     client = pymongo.MongoClient("mongodb://admin:admin123@cluster0-shard-00-00-46e5h.mongodb.net:27017,cluster0-shard-00-01-46e5h.mongodb.net:27017,cluster0-shard-00-02-46e5h.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
-    CreateUser(client, 'adam', 32, 'john@gmail.com')
+    # CreateUser(client, 'adam1', 32, 'john@gmail.com')
+    GetOne(client,'rina')
 except pymongo.errors.ConnectionFailure:
     print("exception")
